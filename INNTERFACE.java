69c9b010390b27db5ef6531d495b2dcd175ceed6
RIFFAAT <rh6565922@gmail.com>
2026-05-28 19:20:41 +0600

    ADDING ANIMATION,DESIGN,COLOR, LAYOUT,ETC
    I AM ADDING ANIMATION, DESIGN, AND COLOR TO THE LEFT AND RIGHT SIDES BEFORE CREATING THE LOGIN INTERFACE

--- INNTERFACE.java
+++ INNTERFACE.java
@@ -1,7 +1,32 @@
 // RIFAT WILL START HIS WORK AFTER THIS LINE:
+import javafx.animation.FadeTransition;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 
 
+public class INTERFACE extends Application {
 
+    private VBox loginBox;
+    private Label titleLabel;
+    private Label descLabel;
+
+
+@Override
+public void start(Stage primaryStage) {
+    HBox root = new HBox();
+    root.setStyle("-fx-background-color: #1f1f1f;");
+    VBox leftPanel = new VBox(20);
+    leftPanel.setPrefWidth(320);
+    leftPanel.setPadding(new Insets(40));
+    leftPanel.setStyle(
+            "-fx-background-color: linear-gradient(to bottom right, #00A8FF, #005CFF);" +
+           "-fx-background-radius: 0;"
+    );
 
 
 
