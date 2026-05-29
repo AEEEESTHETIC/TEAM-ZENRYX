dd176d38dea9c93145a6c21f2d6b11e718fa8295
noushin1234 <hasnahananowshin65@gmail.com>
2026-05-29 19:06:04 +0600

    Working forgot password interface

--- INNTERFACE.java
+++ INNTERFACE.java
@@ -14,7 +14,7 @@ public class INTERFACE extends Application {
     private VBox loginBox;
     private Label titleLabel;
     private Label descLabel;
-
+    private VBox forgotBox;
 
 @Override
 public void start(Stage primaryStage) {
@@ -85,3 +85,16 @@ public void start(Stage primaryStage) {
 
 
 //NOWSHIN WILL START HER WORK AFTER THIS LINE:
+    forgotBox = new VBox(20);
+    forgotBox.setAlignment(Pos.CENTER_LEFT);
+    forgotBox.setMaxWidth(420);
+    forgotBox.setVisible(false);
+
+    Label forgotTitle = new Label("𝗙𝗢𝗥𝗚𝗢𝗧 𝗣𝗔𝗦𝗦𝗪𝗢𝗥𝗗");
+    forgotTitle.setTextFill(Color.web("#00A8FF"));
+    forgotTitle.setFont(Font.font("Arial", FontWeight.NORMAL, 30));
+
+     Label forgotDesc = new Label(
+        "𝗘𝗡𝗧𝗘𝗥 𝗬𝗢𝗨𝗥 𝗘𝗠𝗔𝗜𝗟 𝗔𝗗𝗗𝗥𝗘𝗦𝗦\n𝗧𝗢 𝗥𝗘𝗦𝗘𝗧 𝗣𝗔𝗦𝗦𝗪𝗢𝗥𝗗."
+);
+    
